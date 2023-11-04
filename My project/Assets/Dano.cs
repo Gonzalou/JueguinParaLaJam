c8652e15b5ea3acd113abf69625e6d8b3e1b9b2b
Gonzalou <gonzagtf12@gmail.com>
2023-11-04 17:15:53 -0300

    stsduj

--- My project/Assets/Dano.cs
+++ My project/Assets/Dano.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Dano : MonoBehaviour
 {
     public AudioSource SonidoDeDaño;
-    
+    public int life;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +16,18 @@ public class Dano : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer==12)
+        {
+            SonidoDeDaño.Play();
+            life -= 1;
+        }
+        if (life<=0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
