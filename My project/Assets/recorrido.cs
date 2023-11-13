0919ff60dc157735065dfc40d11ad4cd1ad2b2ac
Doublepinnk <nsoyarce@gmail.com>
2023-11-13 02:03:18 -0300

    trafico arriba y abajo

--- My project/Assets/recorrido.cs
+++ My project/Assets/recorrido.cs
@@ -8,13 +8,27 @@ public class Recorrido : MonoBehaviour
     public float limiteSuperior = 5f; // Ajusta el límite superior del movimiento
     public float limiteInferior = -5f; // Ajusta el límite inferior del movimiento
 
-    private bool moviendoArriba = true;
+    private bool moviendoArriba;
+
+    private float posicionInicialY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionInicialY = transform.position.y;
+
+        // Chequea origen del objeto para saber qué dirección tomar
+        if (posicionInicialY > -421f)
+        {
+            moviendoArriba = true; // Inicia moviéndose hacia arriba
+        }
+        else
+        {
+            moviendoArriba = false; // Inicia moviéndose hacia abajo
+        }
+    }
 
     // Update is called once per frame
-    private void Start()
-    {
-        limiteSuperior = transform.position.y;
-    }
     void Update()
     {
         // Calcula el desplazamiento en el eje Y
@@ -25,9 +39,17 @@ public class Recorrido : MonoBehaviour
         {
             transform.Translate(0f, desplazamientoY, 0f);
 
-            // Si alcanza el límite superior, invierte la dirección
+            // Si alcanza el límite superior, teletransporta a la posición inicial y cambia la dirección a hacia abajo
             if (transform.position.y > limiteSuperior)
             {
+                if (posicionInicialY > -421f)
+                {
+                    transform.position = new Vector3(transform.position.x, posicionInicialY, transform.position.z);
+                }
+                else
+                {
+                    transform.position = new Vector3(transform.position.x, -421f, transform.position.z);
+                }
                 moviendoArriba = false;
             }
         }
@@ -35,21 +57,27 @@ public class Recorrido : MonoBehaviour
         {
             transform.Translate(0f, -desplazamientoY, 0f);
 
-            // Si alcanza el límite inferior, teletransporta a la posición inicial
+            // Si alcanza el límite inferior, teletransporta a la posición inicial y cambia la dirección a hacia arriba
             if (transform.position.y < limiteInferior)
             {
-                transform.position = new Vector3(transform.position.x, limiteSuperior, transform.position.z);
-            }
-            else if (transform.position.y > limiteSuperior)
-            {
-                moviendoArriba = true; // Invierte la dirección si no está teletransportando
+                if (posicionInicialY > -421f)
+                {
+                    transform.position = new Vector3(transform.position.x, posicionInicialY, transform.position.z);
+                }
+                else
+                {
+                    transform.position = new Vector3(transform.position.x, -421f, transform.position.z);
+                }
+                moviendoArriba = true;
             }
         }
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer ==0) 
-            
-        { velocidad = 0; }
+        if (collision.gameObject.layer == 0)
+        {
+            velocidad = 0;
+        }
     }
 }
